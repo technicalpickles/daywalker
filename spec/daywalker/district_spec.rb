a2fadee9272b49701bdf98736d3ef252e53da3d4
Josh Nichols <josh@technicalpickles.com>
2009-02-03 20:19:48 -0500

    Reorganized descriptions.

--- spec/daywalker/district_spec.rb
+++ spec/daywalker/district_spec.rb
@@ -10,28 +10,31 @@ describe Daywalker::District do
     Daywalker.api_key = nil
   end
 
-  describe 'find_by_latlng happy path' do
-    before do
-      FakeWeb.register_uri('http://services.sunlightlabs.com/api/districts.getDistrictFromLatLong.xml?apikey=redacted&latitude=40.739157&longitude=-73.990929', :response => fixture_path_for('district_by_latlng.xml'))
+  describe 'find_by_latlng' do
+    describe 'happy path' do
+      before do
+        FakeWeb.register_uri('http://services.sunlightlabs.com/api/districts.getDistrictFromLatLong.xml?apikey=redacted&latitude=40.739157&longitude=-73.990929', :response => fixture_path_for('district_by_latlng.xml'))
 
+      end
+
+      subject { Daywalker::District.find_by_latlng(40.739157, -73.990929) } 
+
+      specify { subject.size == 1 }
+      specify { subject.first.state.should == 'NY' }
+      specify { subject.first.number.should == 14 }
     end
 
-    subject { Daywalker::District.find_by_latlng(40.739157, -73.990929) } 
+    describe 'bad api key' do
+      before do
+        FakeWeb.register_uri('http://services.sunlightlabs.com/api/districts.getDistrictFromLatLong.xml?apikey=redacted&latitude=40.739157&longitude=-73.990929', :response => fixture_path_for('district_by_latlng_bad_api.xml'))
+      end
 
-    specify { subject.size == 1 }
-    specify { subject.first.state.should == 'NY' }
-    specify { subject.first.number.should == 14 }
-  end
+      specify 'should raise Daywalker::InvalidApiKey' do
+        lambda {
+          Daywalker::District.find_by_latlng(40.739157, -73.990929)
+        }.should raise_error(Daywalker::BadApiKey)
+      end
 
-  describe 'find_by_latlng with bad api key' do
-    before do
-      FakeWeb.register_uri('http://services.sunlightlabs.com/api/districts.getDistrictFromLatLong.xml?apikey=redacted&latitude=40.739157&longitude=-73.990929', :response => fixture_path_for('district_by_latlng_bad_api.xml'))
-    end
-
-    specify 'should raise Daywalker::InvalidApiKey' do
-      lambda {
-        Daywalker::District.find_by_latlng(40.739157, -73.990929)
-      }.should raise_error(Daywalker::BadApiKey)
     end
 
 
