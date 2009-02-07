71ed07d1c679afb9c235781a639ee24799a6c93b
Josh Nichols <josh@technicalpickles.com>
2009-02-06 19:04:41 -0500

    Forgot to add file.

---
+++ lib/daywalker/type_converter.rb
@@ -0,0 +1,47 @@
+module Daywalker
+  class TypeConverter
+    def self.gender_letter_to_sym(letter)
+      case letter
+      when 'M' then :male
+      when 'F' then :female
+      else raise "unknown gender #{letter.inspect}"
+      end
+    end
+
+    def self.party_letter_to_sym(letter)
+      case letter
+      when 'D' then :democrat
+      when 'R' then :republican
+      when 'I' then :independent
+      else raise "Unknown party #{letter.inspect}"
+      end
+    end
+
+    def self.title_abbr_to_sym(abbr)
+      case abbr
+      when 'Sen' then :senator
+      when 'Rep' then :representative
+      else raise "Unknown title #{abbr.inspect}"
+      end
+    end
+
+    def self.sym_to_title_abbr(sym)
+      case sym
+      when :senator then 'Sen'
+      when :representative then 'Rep'
+      else raise "Unknown title #{abbr.inspect}"
+      end
+    end
+
+    def self.blank_to_nil(str)
+      str == '' ? nil : str
+    end
+
+    def self.convert_conditions(conditions)
+      if conditions[:title].kind_of? Symbol
+        conditions[:title] = sym_to_title_abbr(conditions[:title])
+      end
+      conditions
+    end
+  end
+end
